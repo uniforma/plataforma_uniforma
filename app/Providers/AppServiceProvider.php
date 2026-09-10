5dca072ad312a20ad17d5fe510afaf509f5f5046
Matheus Sousa <matheussousab2014@gmail.com>
2026-09-10 09:48:24 -0300

    refactor: remover contratos e repositórios não utilizados no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use App\Repositories\Contracts\BaseContract;
-use App\Repositories\Contracts\DemandContract;
-use App\Repositories\Contracts\SubmissaoContract;
 use App\Repositories\Eloquent\BaseRepository;
-use App\Repositories\Eloquent\DemandRepository;
-use App\Repositories\Eloquent\SubmisssaoRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
